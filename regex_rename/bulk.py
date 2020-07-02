2638512a24a8e18722291a35de1a41723def232f
igrek51 <igrek51.dev@gmail.com>
2020-07-03 00:33:32 +0200

    Improve logging

--- regex_rename/bulk.py
+++ regex_rename/bulk.py
@@ -9,24 +9,20 @@ from nuclear.sublog import log, log_error
 def bulk_rename(pattern: str, replacement: Optional[str], rename: bool, full: bool):
     with log_error():
         testing = not rename
-        if testing:
-            if replacement:
-                log.info('testing replacement pattern', pattern=pattern, replacement=replacement, full_match=full)
-            else:
-                log.info('testing pattern matching', pattern=pattern, full_match=full)
+        log.debug('matching regex pattern', testing_mode=testing, pattern=pattern, replacement=replacement, full_match=full)
 
-        filenames = [str(f) for f in Path().iterdir()]
+        filenames = sorted([str(f) for f in Path().iterdir()])
         matched = sum([match_filename(filename, pattern, replacement, full, testing) for filename in filenames])
 
         if testing:
-            log.info('files matched', count=matched)
+            log.debug('files matched', count=matched)
         else:
             log.info('files renamed', count=matched)
 
 
 def match_filename(filename: str, pattern: str, replacement: Optional[str], full: bool, testing: bool) -> bool:
     if full:
-        match = re.match(pattern, filename)
+        match = re.fullmatch(pattern, filename)
     else:
         match = re.search(pattern, filename)
 
@@ -38,18 +34,18 @@ def match_filename(filename: str, pattern: str, replacement: Optional[str], full
 
 def filename_pattern_testing(filename: str, pattern: str, replacement: Optional[str], match: re.Match) -> bool:
     if not match:
-        log.warn('pattern not matched', filename=filename)
+        log.warn('not matched', file=filename)
         return False
 
     groups = match.groups()
     group_kwargs = {f'group_{idx + 1}': group for idx, group in enumerate(groups)}
 
     if not replacement:
-        log.info('pattern matched', filename=filename, **group_kwargs)
+        log.info('matched', file=filename, **group_kwargs)
         return True
 
     new_name = re.sub(pattern, replacement, filename)
-    log.info('pattern matched', from_name=filename, to_name=new_name, **group_kwargs)
+    log.info('matched', **{'from': filename, 'to': new_name}, **group_kwargs)
     return True
 
 
@@ -58,6 +54,6 @@ def filename_pattern_rename(filename: str, pattern: str, replacement: Optional[s
         return False
 
     new_name = re.sub(pattern, replacement, filename)
-    log.info('renaming file', from_name=filename, to_name=new_name)
+    log.info('renaming file', **{'from': filename, 'to': new_name})
     os.rename(filename, new_name)
     return True
--- regex_rename/main.py
+++ regex_rename/main.py
@@ -5,10 +5,11 @@ from .version import __version__
 
 
 def main():
-    CliBuilder('regex-rename', version=__version__, help='Regular expressions bulk rename tool for multiple files',
-               run=bulk_rename, help_on_empty=True).has(
+    CliBuilder('regex-rename', version=__version__, run=bulk_rename, help_on_empty=True,
+               help='Regular expressions bulk rename tool for multiple files').has(
         argument('pattern', help='Regex pattern to match filenames'),
-        argument('replacement', required=False, help='Replacement regex pattern for renamed files'),
-        flag('rename', help='does actual renaming files instead of just testing replacement pattern'),
-        flag('full', help='enforces matching full filename to pattern'),
+        argument('replacement', required=False, help='Replacement regex pattern for renamed files. '
+                                                     'Use \\1, \\2 syntax to make use of matched groups'),
+        flag('rename', help='Does actual renaming files instead of just testing replacement pattern'),
+        flag('full', help='Enforces matching full filename against pattern'),
     ).run()
