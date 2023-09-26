d4969d3cff837c2fd681584b83a57760d96f1752
igrek51 <igrek51.dev@gmail.com>
2023-09-26 23:13:29 +0200

    Fix unit tests

--- tests/test_duplicates.py
+++ tests/test_duplicates.py
@@ -5,7 +5,7 @@ import pytest
 from regex_rename.rename import bulk_rename
 
 
-def test_no_replacement_nor_testing():
+def test_duplicates_after_replacement():
     cwd = os.getcwd()
     try:
         os.chdir('tests/res/2mp3')
@@ -13,7 +13,7 @@ def test_no_replacement_nor_testing():
         with pytest.raises(RuntimeError) as excinfo:
             bulk_rename(r'(\d+)\.(.+)', r'0.mp3', dry_run=True)
 
-        assert 'found duplicate filenames after replacement: [\'0.mp3\']' in str(excinfo.value)
+        assert 'aborting - found duplicate filenames after replacement: 0.mp3' in str(excinfo.value)
 
     finally:
         os.chdir(cwd)
--- tests/test_stdin_match.py
+++ tests/test_stdin_match.py
@@ -8,9 +8,9 @@ def test_match_files_from_stdin():
         # find -name '_*.mp3'
         shell("printf './res/2mp3/_1.mp3\n./res/2mp3/_2.mp3' | regex-rename '_(.*).mp3'")
     assert_multiline_match(cap.uncolor(), r'''
-\[.*\] DEBUG matching regex pattern pattern=_\(\.\*\).mp3 replacement=None dry_run=True full_match=False recursive=False padding=None
+\[.*\] DEBUG matching regular expression pattern to files: pattern=_\(\.\*\).mp3 replacement=None dry_run=True full_match=False recursive=False padding=None
 \[.*\] DEBUG reading input files from stdin
-\[.*\] INFO  matched file file=res/2mp3/_1.mp3 group_1=1
-\[.*\] INFO  matched file file=res/2mp3/_2.mp3 group_1=2
-\[.*\] INFO  files matched count=2
+\[.*\] INFO  matched file: file=res/2mp3/_1.mp3 group_1=1
+\[.*\] INFO  matched file: file=res/2mp3/_2.mp3 group_1=2
+\[.*\] INFO  files matched the pattern: matched=2 mismatched=0
 '''.strip())
